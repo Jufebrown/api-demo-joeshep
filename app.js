e89eed86b5983595ce7997a96400c2b4c4058345
Jufebrown <jufebrown@gmail.com>
2017-04-17 11:56:38 -0500

    wrote first test

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ var routes = require('./routes/');
 
 var app = express();
 
+// This 'if' statement prevents application log messages from
+// displaying in the stdout when the tests are run
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// allows you to maintain v1 add another app.use to let clients get v2+
+app.use('/api/v1/', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -16,9 +25,32 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+// only sends error stack when in test or dev
+if(app.get('env') === 'development' || app.get('env') === 'test') {
+  app.use((err, req, res, next) => {
+    console.log('error!', err)
+    res.status(err.status || 500)
+    res.json({
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// sends error message but not whole stack
+app.use((err, req, res, next) => {
+  console.log('error!', err)
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    error: {}
+  })
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port} in this super keen env: ${process.env.NODE_ENV}`);
 });
 
+// export app so we can pull in this instance of express into test suite
 module.exports = app;
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,2 +1,35 @@
 // By default, Mocha searches for tests with a “test” folder.
 // That configuration can be changed with a mocha.opts file
+
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const should = chai.should()
+const chaiHttp = require('chai-http')
+const server = require('../app')
+const knex = require('../db/database')
+chai.use(chaiHttp)
+
+// overall describe block
+describe('Shows routes', () => {
+  // before it runs test it will rollback, migrate and seed
+  beforeEach(() => {
+    return knex.migrate.rollback()
+  })
+  .then(() => {
+    return knex.migrate.latest()
+  })
+  .then(() => {
+    return knex.seed.run()
+  })
+  // describe block for getting all shows
+  describe('Get all the shows', () => {
+    it('should get all shows', () => {
+      return chai.request(server)
+      .get('/api/v1/shows')
+      .then((res) => {
+        res.should.have.status(200)
+      })
+    })
+  })
+})
