18d71b4fdeb852610ade29e666406e5b5dce9bb8
Jufebrown <jufebrown@gmail.com>
2017-04-20 09:51:27 -0500

    changed to arrow functions

--- db/migrations/20170417110617_shows.js
+++ db/migrations/20170417110617_shows.js
@@ -1,13 +1,13 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('shows', function(table) {
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('shows', (table) => {
     table.increments()
     table.string('name').notNullable().unique()
     table.string('channel').notNullable()
     table.string('genre').notNullable()
     table.boolean('inProduction').notNullable()
   })
-  .createTable('favorites', function(table) {
+  .createTable('favorites', (table) => {
     table.increments()
     table.timestamp('dateAdded').notNullable().defaultTo(knex.fn.now())
     table.integer('show_id').unsigned().references('shows.id')
