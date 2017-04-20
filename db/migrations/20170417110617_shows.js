4868b908896cdc400fe7517681934372aacff481
Jufebrown <jufebrown@gmail.com>
2017-04-20 09:51:55 -0500

    added 2 tables to db

--- db/migrations/20170417110617_shows.js
+++ db/migrations/20170417110617_shows.js
@@ -12,6 +12,18 @@ exports.up = (knex, Promise) => {
     table.timestamp('dateAdded').notNullable().defaultTo(knex.fn.now())
     table.integer('show_id').unsigned().references('shows.id')
   })
+  .createTable('directors', (table) => {
+    table.increments()
+    table.string('name').notNullable().unique()
+    table.string('gender').notNullable()
+    table.integer('birthYear')
+    table.string('twitterHandle')
+  })
+  .createTable('shows_directors', (table) => {
+    table.increments()
+    table.integer('director_id').unsigned().references('directors.id')
+    table.integer('show_id').unsigned().references('shows.id')
+  })
 };
 
 exports.down = (knex, Promise) => knex.schema.dropTable('favorites').dropTable('shows')
