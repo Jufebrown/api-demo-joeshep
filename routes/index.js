382743cc6e7f282a79c15f9f4d454608a5f4fdeb
Jufebrown <jufebrown@gmail.com>
2017-04-20 12:44:28 -0500

    fixed routes list on index.js

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,12 @@ router.use(require('./directors'))
 router.get('/', function(req, res) {
   res.json({
     'shows': 'https://api-demo-jufe.herokuapp.com/api/v1/shows',
-    'favorites': 'https://api-demo-jufe.herokuapp.com/api/v1/favorites?showId=<showId>',
-    'directors': 'https://api-demo-jufe.herokuapp.com/api/v1/directors?showId=<showId>'
+    'show': 'https://api-demo-jufe.herokuapp.com/api/v1/shows/:showId',
+    'favorites': 'https://api-demo-jufe.herokuapp.com/api/v1/shows/favorites?showId=<showId>',
+    'showAndItsDirectors': 'https://api-demo-jufe.herokuapp.com/api/v1/shows/directors?showId=<showId>',
+    'directors': 'https://api-demo-jufe.herokuapp.com/api/v1/directors',
+    'director': 'https://api-demo-jufe.herokuapp.com/api/v1/directors/:directorId'
+    'directorAndTheirShows': 'https://api-demo-jufe.herokuapp.com/api/v1/directors/shows?directorId=<directorId>',
   })
 })
 
